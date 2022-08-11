0afbb2bfcbb838dfbaa8aa56d2628536fd160560
Anastasiia-Svechnikova <anastasiiakundriukova@ymail.com>
2022-08-11 12:11:19 +0300

    task 2 refactored

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,9 +1,9 @@
+import convertMs from './convertms.js';
 import flatpickr from 'flatpickr';
-require('flatpickr/dist/themes/confetti.css');
+import 'flatpickr/dist/themes/confetti.css';
 
 import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const startBtnref = document.querySelector('[data-start]');
 const fieldRefs = {
@@ -22,14 +22,12 @@ const options = {
   minuteIncrement: 1,
 
   onClose(selectedDates) {
-    const today = new Date();
     const selectedDateUnix = selectedDates[0].getTime();
     let id = null;
 
-    if (today.getTime() > selectedDateUnix) {
-      console.log(
-        Notiflix.Notify.failure('Please choose a date in the future')
-      );
+    if (Date.now() > selectedDateUnix) {
+      Notiflix.Notify.failure('Please choose a date in the future');
+
       startBtnref.disabled = true;
       return;
     }
@@ -45,8 +43,7 @@ const options = {
     }
 
     function getDelta() {
-      const today = new Date();
-      const delta = convertMs(selectedDateUnix - today.getTime());
+      const delta = convertMs(selectedDateUnix - Date.now());
 
       renderTimer(delta);
     }
@@ -56,25 +53,6 @@ const options = {
 };
 
 flatpickr('#datetime-picker', options);
-
-function convertMs(ms) {
-  // Number of milliseconds per unit of time
-  const second = 1000;
-  const minute = second * 60;
-  const hour = minute * 60;
-  const day = hour * 24;
-
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-  return { days, hours, minutes, seconds };
-}
 
 function renderTimer(date) {
   Object.keys(date).forEach(
